3388cb3e0ec6145a3147ba096dc211ec7377abf3
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-03 11:20:10 +0300

    sidebar columns eklendi

--- src/views/Layouts/Sidebar.vue
+++ src/views/Layouts/Sidebar.vue
@@ -1,13 +1,119 @@
 <template>
   <nav class="sidebar col-md-12 col-lg-4 col-xl-4 col-xxl-3">
-    <h1>sidebar</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+          <img
+            class="image"
+            src="../../assets/img/sidebar/sidebar.img.svg"
+            alt=""
+          />
+        </div>
+        <div class="col d-flex justify-content-center">
+          <p class="title">Akgül Yılmaz</p>
+        </div>
+        <span>GREY LINE</span>
+        <div class="col cols d-flex justify-content-start align-items-center">
+          <img src="../../assets/img/sidebar/giris.svg" alt="" class="logo" />
+          <div class="text">Giriş</div>
+        </div>
+        <!-- col ends -->
+        <div class="col cols d-flex justify-content-start align-items-center">
+          <img
+            src="../../assets/img/sidebar/randevularim.svg"
+            alt=""
+            class="logo"
+          />
+          <div class="text">Randevularım</div>
+        </div>
+        <!-- col ends -->
+        <div class="col cols d-flex justify-content-start align-items-center">
+          <img
+            src="../../assets/img/sidebar/kayitlar.svg"
+            alt=""
+            class="logo"
+          />
+          <div class="text">Tıbbi Kayıtlar</div>
+        </div>
+        <!-- col ends -->
+        <div class="col cols d-flex justify-content-start align-items-center">
+          <img src="../../assets/img/sidebar/gecmis.svg" alt="" class="logo" />
+          <div class="text">Geçmiş Muayeneler</div>
+        </div>
+        <!-- col ends -->
+        <div class="col cols d-flex justify-content-start align-items-center">
+          <img
+            src="../../assets/img/sidebar/receteler.svg"
+            alt=""
+            class="logo"
+          />
+          <div class="text">Reçeteler</div>
+        </div>
+        <!-- col ends -->
+        <div class="col cols d-flex justify-content-start align-items-center">
+          <img src="../../assets/img/sidebar/hesap.svg" alt="" class="logo" />
+          <div class="text">Hesap Bilgileriniz</div>
+        </div>
+        <!-- col ends -->
+        <div class="col cols d-flex justify-content-start align-items-center">
+          <img src="../../assets/img/sidebar/anket.svg" alt="" class="logo" />
+          <div class="text">Anketimize katılın</div>
+        </div>
+        <!-- col ends -->
+        <div class="col cols d-flex justify-content-start align-items-center">
+          <img src="../../assets/img/sidebar/cikis.svg" alt="" class="logo" />
+          <div class="text">Çıkış Yap</div>
+        </div>
+        <!-- col ends -->
+      </div>
+    </div>
   </nav>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
+<style scoped lang="scss">
+@import "../../assets/scss/style";
+.sidebar {
+  width: 270px;
+  height: 1018px;
+  background: $sidebar;
+  border-radius: 18px 0px 0px 18px;
+}
 
-<style scoped>
-
+.image {
+  width: 116px;
+  height: 116px;
+}
+.logo {
+  margin-right: 10px;
+  width: 22px;
+  height: 22px;
+}
+.title {
+  width: 97px;
+  height: 22px;
+  font-family: Nunito Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 22px;
+  /* identical to box height */
+  text-align: center;
+  color: $title;
+}
+.text {
+  font-family: Nunito Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 22px;
+  /* identical to box height */
+  color: $text;
+  width: auto;
+  height: 22px;
+}
+.cols {
+  margin-bottom: 28px;
+}
 </style>
