2c66a588d41bd066d76eba751f416c4c3b161658
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-03 13:26:18 +0300

    fallback

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,16 @@
 <template>
-  <h1>dashboard</h1>
+  <div class="dashboard"></div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped lang="scss">
 @import "../assets/scss/style";
+.dashboard {
+  width: 100%;
+  height: 100%;
+  background: $main;
+  box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.15);
+  border-radius: 18px;
+}
 </style>
--- src/views/Layouts/Main.vue
+++ src/views/Layouts/Main.vue
@@ -6,10 +6,8 @@
 
 <script>
 export default {
-  name: "Main"
-}
+  name: "Main",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
