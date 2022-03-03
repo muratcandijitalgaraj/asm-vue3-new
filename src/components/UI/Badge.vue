0a32e885ce888852c12aa5f79b37b1f4a09b2a3d
Gökhan ALKAN <info@gokhanalkan.com>
2022-03-03 15:10:32 +0300

    Update Badge.vue

--- src/components/UI/Badge.vue
+++ src/components/UI/Badge.vue
@@ -1,10 +1,14 @@
 <template>
-
+  <span class="badge" :class="type"> {{ number }}</span>
 </template>
 
 <script setup>
+const props = defineProps({
+  number: {required: true, type: Number},
+  type: {required: false, type: String, default: 'danger'}
+})
 </script>
 
 <style scoped lang="scss">
-
+@import "../../assets/scss/style";
 </style>
