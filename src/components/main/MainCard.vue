edf5b6b7be096b940e5c1cf474250e65ff8c21a8
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-04 13:06:26 +0300

    kartlar eklendi

--- src/components/main/MainCard.vue
+++ src/components/main/MainCard.vue
@@ -6,10 +6,13 @@
     </router-link>
   </li> -->
   <div class="mainCard">
-    <router-link :to="url" class="">
-      <span :class="lineColor"></span>
+    <router-link
+      :class="color"
+      :to="url"
+      class="d-flex flex-column align-items-start jutify-content-center"
+    >
       <img :src="src" :alt="title" class="logo" />
-      <span class="text">lol + {{ title }}</span>
+      <span class="title text">{{ title }}</span>
     </router-link>
   </div>
 </template>
@@ -19,6 +22,7 @@ const props = defineProps({
   src: { required: true, type: String },
   title: { required: true, type: String },
   url: { required: true, type: Object },
+  color: { required: true, type: String },
 });
 </script>
 
@@ -32,6 +36,15 @@ const props = defineProps({
   /* Boxx Shadow */
   box-shadow: 0px 1px 3px rgba(42, 49, 55, 0.11);
   border-radius: 0px 0px 10px 10px;
-  border: solid;
+}
+.logo {
+  margin-bottom: 10px;
+  margin-top: 14px;
+}
+.text {
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 22px;
+  color: $text;
 }
 </style>
--- src/views/Layouts/Main.vue
+++ src/views/Layouts/Main.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="main-container col-md-12 col-lg-8 col-xl-8 col-xxl-9">
     <!-- somehow, this is the section slot in master -->
-    <div class="row d-flex">
+    <div class="row d-flex justify-content-evenly">
       <MainCard
         v-for="(item, key) in cards"
         :title="item.title"
         :url="item.url"
         :src="item.src"
         :key="key"
+        :class="item.color"
       />
     </div>
   </div>
@@ -17,9 +18,30 @@
 import MainCard from "../../components/main/MainCard.vue";
 import { ref } from "vue";
 const cards = ref([
-  { src: "/src/assets/img/sidebar/giris.svg", title: "Giriş", url: {} },
-  { src: "/src/assets/img/sidebar/giris.svg", title: "Giriş", url: {} },
-  { src: "/src/assets/img/sidebar/giris.svg", title: "Giriş", url: {} },
+  {
+    src: "/src/assets/img/cards/blue.svg",
+    title: "Hastane Randevusu Alın",
+    url: {},
+    color: "blue box",
+  },
+  {
+    src: "/src/assets/img/cards/orange.svg",
+    title: "Görüntülü Görüşme Randevusu Alın",
+    url: {},
+    color: "orange box",
+  },
+  {
+    src: "/src/assets/img/cards/purple.svg",
+    title: "Check-Up Başvurusu Yapın",
+    url: {},
+    color: "purple box",
+  },
+  {
+    src: "/src/assets/img/cards/green.svg",
+    title: "Destek Merkezi",
+    url: {},
+    color: "green box",
+  },
 ]);
 </script>
 
@@ -30,5 +52,20 @@ const cards = ref([
   background: $main;
   border-top-right-radius: 18px;
   border-bottom-right-radius: 18px;
+}
+.box {
+  margin-top: 29px;
+}
+.blue {
+  border-top: 3px solid $blue;
+}
+.orange {
+  border-top: 3px solid $orange;
+}
+.purple {
+  border-top: 3px solid $purple;
+}
+.green {
+  border-top: 3px solid $green;
 }
 </style>
