791b5a8f2aa6cf4727d5c9f921962f01b0c94dd0
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-15 14:19:51 +0300

    refactored the template.

--- src/components/randvularimPage/TopPart.vue
+++ src/components/randvularimPage/TopPart.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="randevularimTop">
-    <span class="bigTitle">Randevularım</span>
+  <div class="row topContainer">
+    <div class="col-12 col-md-8 leftPart d-flex">
+      <span class="col-12 col-md-4 bigTitle">Randevularım</span>
+      <span class="col-12 col-md-4 title">Yerinde Randevular</span>
+      <span class="col-12 col-md-4 title">Görüntülü Görüşme</span>
+    </div>
+    <button class="btn col-12 col-md-4 d-flex justify-content-end">
+      <span class="btnPara">Yeni Randevu Alın</span>
+    </button>
   </div>
 </template>
 
@@ -8,4 +15,11 @@
 
 <style scoped lang="scss">
 @import "../../assets/scss/style";
+.lineContainer {
+  width: 100%;
+  border: 1px solid;
+}
+.blueLine {
+  border: 2px solid;
+}
 </style>
