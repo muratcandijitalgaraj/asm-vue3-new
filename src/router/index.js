cffa024ea296e15e3edd8637d56e25b402243733
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-16 12:23:27 +0300

    fallback

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/",
+    path: "/anasayfa",
     name: "Dashboard",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
