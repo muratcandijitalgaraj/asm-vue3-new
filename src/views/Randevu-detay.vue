1080dbfa4fbeda7c6f3d243a858c5128207831d9
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-16 13:26:45 +0300

    start with randevu detay

---
+++ src/views/Randevu-detay.vue
@@ -0,0 +1,12 @@
+<template>
+  <Master>
+    <template v-slot:header><Header /></template>
+    <template v-slot:sidebar><Sidebar /></template>
+    <template v-slot:section> <!-- <Main> </Main> -->detay </template>
+    <template v-slot:footer><Footer /></template>
+  </Master>
+</template>
+
+<script setup></script>
+
+<style></style>
