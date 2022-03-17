8d2b6d4c6534083610151f21933f9557c699e8ad
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-17 11:27:01 +0300

    adding randevu detay cards, fallback

---
+++ src/components/UI/RandevuDetay/Cards.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="detayCards d-flex flex-column">
+    <div class="cardContainer d-flex">
+      <img class="logo" :src="src" alt="" />
+      <div class="texts d-flex flex-column">
+        <span class="para">{{ para }} </span>
+        <span class="title"> {{ title }} </span>
+      </div>
+    </div>
+    <div v-if="type === 1" class="greyLine"></div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  src: { required: true, type: String },
+  para: { required: true, type: String },
+  title: { required: true, type: String },
+  type: { required: true, type: Number },
+});
+</script>
+
+<style scoped lang="scss">
+@import "../../../assets/scss/style";
+.logo {
+}
+.para {
+}
+.title {
+}
+.greyLine {
+}
+</style>
