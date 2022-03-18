ee513022d6437f897f5bcc688ab347266dc4e2e1
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-18 14:23:32 +0300

    adding search part

--- src/components/tıbbi-kayıtlar/radyoloji/Radyoloji.vue
+++ src/components/tıbbi-kayıtlar/radyoloji/Radyoloji.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="radyoloji">
     <Top />
+    <Search />
   </div>
 </template>
 
 <script setup>
 import Top from "../../UI/radyoloji/Top.vue";
+import Search from "../../UI/radyoloji/Search.vue";
 </script>
 
 <style scoped lang="scss">
