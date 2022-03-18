d00cc2fdf48d90b9f52cb15b18944982fdc55ec0
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-18 15:48:35 +0300

    fallback

--- src/components/UI/radyoloji/Card.vue
+++ src/components/UI/radyoloji/Card.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="radyolojiBody">
-    <span class="bigTitle"></span>
-  </div>
+  <div class="card"></div>
 </template>
 
 <script setup>
 const props = defineProps({
   type: { required: true },
-  bigTitle: { required: true, type: String },
   logo: { required: true, type: String },
   title: { required: true, type: String },
   para: { required: true, type: String },
@@ -16,18 +13,33 @@ const props = defineProps({
 
 <style scoped lang="scss">
 // @import "../../assets/scss/style";
-.bigTitle {
-  /* baslik3 */
-
+.title {
   font-family: "Nunito Sans";
   font-style: normal;
   font-weight: 700;
-  font-size: 22px;
-  line-height: 130%;
-  /* identical to box height, or 29px */
+  font-size: 16px;
+  line-height: 150%;
+  /* identical to box height, or 24px */
 
-  /* Secondary */
+  /* Primary */
 
+  color: #3c4e69;
+}
+.para {
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 140%;
+  /* identical to box height, or 21px */
+
+  letter-spacing: -0.01em;
+
+  /* Gri */
+
+  color: #818b9a;
+}
+.bluePara {
   color: #32a5df;
 }
 </style>
