7093fd3575aa7ce7c130ffbe4aeff0fbe03908fa
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-19 18:43:57 +0300

    add title

--- src/components/receteler/Receteler.vue
+++ src/components/receteler/Receteler.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="receteler">
+  <div class="receteler d-flex flex-column">
+    <div class="bigTitle">Reçeteler</div>
+    <div class="greyLine"></div>
     <TopVue />
   </div>
 </template>
@@ -10,4 +12,16 @@ import TopVue from "../UI/receteler/Top.vue";
 
 <style scoped lang="scss">
 // @import "../../assets/scss/style";
+.bigTitle {
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 130%;
+  color: #3c4e69;
+  margin-bottom: 25px;
+}
+.greyLine {
+  border: 1px solid #dae9f1;
+}
 </style>
