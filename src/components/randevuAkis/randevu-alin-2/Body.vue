3194c9d68260895e148032c97b3b1294127d4633
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-23 17:23:57 +0300

    randevu akis 2 done

--- src/components/randevuAkis/randevu-alin-2/Body.vue
+++ src/components/randevuAkis/randevu-alin-2/Body.vue
@@ -3,18 +3,59 @@
     <div class="col-12 col-lg-7 left">
       <Left />
     </div>
-    <div class="col-12 col-lg-5 right">
-      <Right />
+
+    <div class="col-12 col-lg-5 right d-flex flex-column align-items-center">
+      <span class="title">Seçimleriniz</span>
+
+      <Right
+        v-for="(item, key) in choices"
+        :key="key"
+        :para="item.para"
+        :logo="item.logo"
+        :color="item.color"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
+import orangeLogo from "../../../assets/img/randevuAkis/video.svg";
+import blackLogo from "../../../assets/img/randevuAkis/user.svg";
+import { ref } from "vue";
 import Left from "./Left.vue";
 import Right from "./Right.vue";
+
+const choices = ref([
+  {
+    para: "Görüntülü Görüşme",
+    logo: orangeLogo,
+    color: "orange",
+  },
+  {
+    para: "Mehmet Yılmaz için",
+    logo: blackLogo,
+    color: "black",
+  },
+]);
 </script>
 
 <style scoped lang="scss">
 .body {
+}
+.title {
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 130%;
+  /* identical to box height, or 22px */
+
+  letter-spacing: -0.01em;
+
+  /* Turuncu */
+  margin-bottom: 21px;
+  margin-top: 30px;
+
+  color: #ff8038;
 }
 </style>
