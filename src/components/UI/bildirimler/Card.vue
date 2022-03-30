03e459b4f243a9cc11e1c9171203c2937cbdde2c
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-30 18:13:36 +0300

    bildirimler detay done

--- src/components/UI/bildirimler/Card.vue
+++ src/components/UI/bildirimler/Card.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="card d-flex flex-row justify-content-between align-items-center">
-    <div class="left d-flex flex-row justify-content-start align-items-center">
-      <div class="logoContainer" :class="bgColor">
+    <div
+      class="left d-flex flex-column flex-xl-row justify-content-start align-items-center"
+    >
+      <div
+        class="logoContainer d-flex justify-content-start align-items-center"
+        :class="bgColor"
+      >
         <img :src="logo" alt="" class="logo" />
       </div>
       <div
@@ -10,8 +15,12 @@
         <span class="title" :class="titleColor"> {{ date }} </span>
         <span class="paraColor"> {{ para }} </span>
         <div v-if="type === 2" class="buttonContainer">
-          <button class="buttonOne">Örnek Buton</button>
-          <button class="buttonTwo">Örnek Buton 2</button>
+          <button class="buttonOne">
+            <span class="buttonOneText">Örnek Buton</span>
+          </button>
+          <button class="buttonTwo">
+            <span class="buttonTwoText">Örnek Buton 2</span>
+          </button>
         </div>
       </div>
     </div>
@@ -38,8 +47,10 @@ const props = defineProps({
   background: #f2f8fb;
   border: none;
   padding-bottom: 20px;
+  margin-bottom: 20px;
   border-bottom: 1px solid #dae9f1;
 }
+
 .paraColor {
   /* baslik4 */
 
@@ -57,14 +68,62 @@ const props = defineProps({
 .logoContainer {
   width: 40px;
   height: 40px;
-  left: 0px;
-  top: 0px;
   border-radius: 6px;
+  margin-right: 15px;
 }
 .blue {
   background: #32a5df;
 }
 .grey {
   background: #ffffff;
+}
+.textBlue {
+  color: #32a5df !important;
+}
+.textGrey {
+  color: #818b9a !important;
+}
+.title {
+  font-family: "Nunito Sans" !important;
+  font-style: normal;
+  font-weight: 700 !important;
+  font-size: 16px !important;
+  line-height: 150% !important;
+  /* identical to box height, or 24px */
+}
+.buttonContainer {
+  margin-top: 20px;
+}
+.buttonOne {
+  padding: 12px 20px 11px;
+  /* Button Primary */
+  background: #ff7c32;
+  border-radius: 6px;
+  border: none;
+  margin-right: 20px;
+}
+.buttonOneText {
+  color: white;
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 110%;
+}
+.buttonTwo {
+  padding: 12px 20px 11px;
+  /* Button Primary */
+  border: 1px solid #ff7c32;
+  box-sizing: border-box;
+  border-radius: 6px;
+  background: white;
+}
+.buttonTwoText {
+  color: #ff7c32;
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 110%;
 }
 </style>
