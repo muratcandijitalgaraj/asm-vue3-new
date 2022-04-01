edd8708bded31b1dce6c05c63ea8d7c8af2bd028
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-01 19:42:40 +0300

    adding fast

--- src/components/puan/Main.vue
+++ src/components/puan/Main.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="body">puans</div>
+  <div class="body d-flex justify-content-center align-items-center">
+    <Puan1 />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Puan1 from "./Puan-1.vue";
+import Puan2 from "./Puan-2.vue";
+</script>
 
-<style scoped></style>
+<style scoped>
+.body {
+  height: 100vh;
+  padding: 2rem;
+}
+</style>
--- src/components/puan/Puan-1.vue
+++ src/components/puan/Puan-1.vue
@@ -1,5 +1,27 @@
-<template></template>
+<template>
+  <div class="body">
+    <div class="closeContainer d-flex justify-content-end">
+      <img :src="close" alt="" class="close" />
+    </div>
+    <div class="likeContainer d-flex justify-content-center">
+      <img :src="like" alt="" class="close" />
+    </div>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import close from "../../assets/img/puan/close.svg";
+import like from "../../assets/img/puan/like.svg";
+import message from "../../assets/img/puan/message.svg";
+import video from "../../assets/img/puan/video.svg";
+import bulutlar from "../../assets/img/puan/bulutlar.svg";
+</script>
 
-<style scoped></style>
+<style scoped>
+.body {
+  width: 675px !important;
+  height: 629px !important;
+  background: #e9f3f9;
+  border-radius: 16px;
+}
+</style>
