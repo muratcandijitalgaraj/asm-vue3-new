30b576d427157940650b7574da1da4d3ae1e5d60
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-02 14:45:03 +0300

    UI done

--- src/components/puan/Puan-1.vue
+++ src/components/puan/Puan-1.vue
@@ -1,26 +1,68 @@
 <template>
   <div class="body">
-    <div class="closeContainer d-flex justify-content-end">
+    <div class="closeContainer d-flex justify-content-end align-items-start">
+      <img :src="bulutlar" alt="" class="bulutlar" />
       <img :src="close" alt="" class="close" />
     </div>
-    <div class="likeContainer d-flex justify-content-center">
+    <!-- <div class="likeContainer d-flex justify-content-center">
       <img :src="like" alt="" class="like" />
-    </div>
+    </div> -->
     <div
-      class="videoContainer d-flex flex-row justify-content-start align-items-center"
+      class="question d-flex flex-row justify-content-start align-items-center"
     >
       <img :src="video" alt="" class="video paraLogo" />
       <span class="title">Görüşme kalitesi nasıldı?</span>
+    </div>
+    <!-- cards -->
+    <div
+      class="cards d-flex flex-row align-items-center justify-content-between"
+    >
+      <div
+        v-for="(card, index) in cards"
+        :key="card.index"
+        class="card d-flex flex-row align-items-center"
+      >
+        <div class="circle"></div>
+        <span class="title"> {{ card.title }} </span>
+      </div>
+    </div>
+    <!-- second question -->
+    <div
+      class="question d-flex flex-row justify-content-start align-items-center"
+    >
+      <img :src="message" alt="" class="message paraLogo" />
+      <span class="title">İletmek istediğiniz not var mı?</span>
+    </div>
+    <input
+      type="text"
+      class="bigInput"
+      placeholder="Dlerseniz boş bırakabilirsiniz."
+    />
+    <div class="buttonContainer d-flex justify-content-center">
+      <button @click="handleButton" class="button">
+        <span class="buttonText">+ Yeni Profil Ekle</span>
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import close from "../../assets/img/puan/close.svg";
 import like from "../../assets/img/puan/like.svg";
 import message from "../../assets/img/puan/message.svg";
 import video from "../../assets/img/puan/video.svg";
 import bulutlar from "../../assets/img/puan/bulutlar.svg";
+
+let title = ref("");
+
+const cards = ref([
+  { title: "Çok iyi" },
+  { title: "İyi" },
+  { title: "Eh İşte" },
+  { title: "Kötü" },
+  { title: "Çok Kötü" },
+]);
 </script>
 
 <style scoped>
@@ -30,15 +72,85 @@ import bulutlar from "../../assets/img/puan/bulutlar.svg";
   background: #e9f3f9;
   border-radius: 16px;
 }
-.title {
+.bulutlar {
+  position: relative;
+  z-index: 1;
+}
+.closeContainer {
+  margin-bottom: 30px;
+}
+/* .title {
   font-family: "Nunito Sans";
   font-style: normal;
   font-weight: 700;
   font-size: 16px;
   line-height: 150%;
   color: #3c4e69;
-}
+} */
 .paraLogo {
   margin-right: 15px;
+}
+.question {
+  margin-bottom: 18px;
+}
+.cards {
+}
+.card {
+  background: #ffffff;
+  /* Boxx Shadow */
+  box-shadow: 0px 1px 3px rgba(42, 49, 55, 0.11);
+  border-radius: 6px;
+  padding: 12px;
+}
+.circle {
+  height: 20px;
+  width: 20px;
+  border: 1px solid #e1e1e1;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+.title {
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 150%;
+  /* identical to box height, or 24px */
+  /* Primary */
+  color: #3c4e69;
+  margin-right: 5px;
+}
+.bigInput {
+  width: 605px;
+  height: 150px;
+  border: none;
+  padding-bottom: 90px !important;
+  padding-left: 10px;
+}
+.buttonContainer {
+  width: 100%;
+  margin-top: 30px;
+}
+.button {
+  padding: 12px 20px 11px;
+  width: 100%;
+  height: 50px;
+  background: #ff8038;
+  border-radius: 6px;
+  border: none;
+}
+.buttonText {
+  /* buton-text-orta */
+
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 16px;
+  line-height: 110%;
+  /* identical to box height, or 18px */
+
+  /* Beyaz */
+
+  color: #ffffff;
 }
 </style>
