8b5d0845f2908b6a1c40b007ff88a9acb745704f
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-06 16:25:21 +0300

    add kayit

--- src/router/index.js
+++ src/router/index.js
@@ -113,6 +113,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Giris.vue"),
   },
+  {
+    path: "/kayit",
+    name: "Kayit",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Kayit.vue"),
+  },
 ];
 
 const router = createRouter({
