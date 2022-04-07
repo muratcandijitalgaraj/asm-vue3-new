685afd1534607b58b05b3546179aa60344bada91
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-07 14:47:54 +0300

    add playground

--- src/router/index.js
+++ src/router/index.js
@@ -122,6 +122,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Kayit.vue"),
   },
+  {
+    path: "/playground",
+    name: "Playground",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Playground.vue"),
+  },
 ];
 
 const router = createRouter({
