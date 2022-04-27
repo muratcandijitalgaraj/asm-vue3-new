0fc3b0561065b8a0a5de473641840cbdbc01dafd
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-27 13:06:16 +0300

    .

--- src/views/RandevuAkis.vue
+++ src/views/RandevuAkis.vue
@@ -18,13 +18,12 @@ import SecimVue from "../components/randevuAkis/Secim.vue";
 
 <style lang="scss" scoped>
 .zemin {
-  width: 100%;
-  height: 788px;
-  left: 612px;
-  top: 91px;
-
+  width: 60%;
+  height: 100%;
   background: #f2f8fb;
   box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.15);
   border-radius: 18px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
 }
 </style>
